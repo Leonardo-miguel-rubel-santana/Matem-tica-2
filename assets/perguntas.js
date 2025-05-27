82020120b4274b417403c468ab3dfdbc2622a6e2
foden doku <leonardo.rubel.santana@escola.pr.gov.br>
2025-05-27 08:46:12 -0300

    banana de pijama

---
+++ assets/perguntas.js
@@ -0,0 +1,18 @@
+criaCartao(
+    'Times de futebol',
+    'Qual time é conhecido como Imortal?',
+  É o Grêmio 
+
+)
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+    
+)
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+    
+)
