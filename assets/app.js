267f8395dfa4bc5e3e5a67ebe305b0f896813b25
foden doku <leonardo.rubel.santana@escola.pr.gov.br>
2025-06-03 20:31:02 -0300

    jj

--- assets/app.js
+++ assets/app.js
@@ -1,21 +1,28 @@
 function criaCartao(categoria, pergunta, resposta){
 let container =document.getElementById('container')
-let.cartao = document.createElement('article')
+let cartao = document.createElement('article')
 cartao.className = 'cartao'
  
-cartao.innerHTML =´
-<articule class="cartao">
+cartao.innerHTML =`
     <div class="cartao__conteudo">
-        <h3>Times de Futebol </h3>
+        <h3>${categoria}</h3>
         <div class="cartao__conteudo__pergunta">
-             <p>Qual o time conhecido como Imortal ?<p>
+             <p>${pergunta}?<p>
         </div>
     <div class="cartao__conteudo__resposta"></div>
-        <p>È o Grêmio<p>
+        <p>${resposta}<p>
     </div>
     </div>
+`
+
+let respostaEstaVisivel = false
+
+function viraCartao(){
+    respostaEstaVisivel = !respostaEstaVisivel
+    cartao.classList.toggle('active', respostaEstaVisivel)
+}
+cartao.addEventListener('click', viraCartao)
+
 
     container.appendChild(cartao)
-´
-
 }
--- assets/perguntas.js
+++ assets/perguntas.js
@@ -2,17 +2,22 @@ criaCartao(
     'Times de futebol',
     'Qual time é conhecido como Imortal?',
   'É o Grêmio '
+)
 
-)
 criaCartao(
-    JOGADOR,
+    'JOGADOR',
    'Qual jogador éconhecido como robô?',
-    'É O Cristiano Ronaldo'
-    
+    'É O Cristiano Ronaldo'  
 )
+
 criaCartao(
-    Mascote,
-    'De qual time é o mascote que é um Urubu?,
-    'È do Flamengo'
-    
+    'Mascote',
+    'De qual time é o mascote que é um Urubu?',
+    'È do Flamengo' 
+)
+
+criaCartao(
+    'Troféus',
+    'Qual time ganhou a libertadores de 2017?',
+    'Foi o Grêmio'
 )
